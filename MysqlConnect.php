b834f7baa3dfa6f2008fbaf353e95a55dabccfc6
tokmendoza <eduardopmendozajr@gmail.com>
2017-10-13 13:42:58 +0800

    deleted

--- MysqlConnect.php
+++ MysqlConnect.php
@@ -2,20 +2,7 @@
 
 
 function myConnect(){
-	$servername = "localhost";
-	$username = "root";
-	$password = "";
-	$db="coffeemgntsys";
-
-	static $conn;
-	$conn = mysqli_connect($servername, $username, $password,$db);
-
-	if (!$conn) {
-		die("Connection failed: " . mysqli_connect_error());
-	}else{
-		//echo "Connected successfully";	
-	}
-	return $conn;
+	
 }
 
 
